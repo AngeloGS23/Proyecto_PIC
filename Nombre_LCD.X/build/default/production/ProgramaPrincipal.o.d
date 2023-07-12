6ca4e3b6086cd7c63fd3ac90bfe8d1cada3c91f0
AngeloGS23 <angelosanchezloja.123@gmail.com>
2023-07-12 18:20:18 -0500

    se agrega el proyecto

---
+++ Nombre_LCD.X/build/default/production/ProgramaPrincipal.o.d
@@ -0,0 +1,4 @@
+build/default/production/ProgramaPrincipal.o:  \
+	C:/Users/Matias/MPLABXProjects/prueba.X/ProgramaPrincipal.asm  \
+	C:/Users/Matias/MPLABXProjects/prueba.X/Libreria.inc  \
+
---
+++ Nombre_LCD.X/build/default/production/programa.o.d
@@ -0,0 +1,4 @@
+build/default/production/programa.o:  \
+	C:/Users/gabri/MPLABXProjects/prueba.X/programa.asm  \
+	C:/Users/gabri/MPLABXProjects/prueba.X/libreria.inc  \
+
